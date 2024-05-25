c59903983fedb6c28a83af33e9584db920a883f1
phanta1313 <93367196+phanta1313@users.noreply.github.com>
2024-05-25 15:29:17 +0300

    Add files via upload

--- todolist/tasks/urls.py
+++ todolist/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-urlpatterns = {
-    path('tasks/show', views.showTasks, name="ShowTasks"),
+urlpatterns = [
+    path('tasks/show', views.showTasks, name="showTasks"),
     path('task/show/<str:pk>/', views.showTask, name="showTask"),
 
-    path('tasks/add', views.createTask, name="createTask"),
-    path('tasks/edit', views.editTask, name="editTask"),
-    path('tasks/add', views.deleteTask, name="deleteTask"),
-}
+    path('task/create', views.createTask, name="createTask"),
+    path('task/edit/<str:pk>', views.editTask, name="editTask"),
+    path('task/delete/<str:pk>', views.deleteTask, name="deleteTask"),
+]
--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,34 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
 def showTasks(request):
+    page = 'showtasks'
     tasks = Task.objects.all()
-    context = {'tasks': tasks}
+    context = {'tasks': tasks, 'page':page}
     return render(request, 'index.html', context)
 def showTask(request,pk):
+    page = 'showtask'
     task = Task.objects.get(pk=pk)
 
-    context = {'task': task}
+    context = {'task': task, 'page':page}
     return render(request, 'index.html', context)
 def createTask(request):
-    task = Task()
-    if request.method == "POST":
-        task = Task()
-        f
-    context = {'task': task}
-    return render(request, 'index.html', context)
-def editTask(request):
-    
-    task = Task()
-    if request.method == "PUT":
-        task = Task()
+    page = 'createtask'
+    form = TaskForm(request.POST, request.FILES)
 
-    context = {'task': task}
+    if request.method == "POST":
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            redirect('showTasks')
+
+    context = {'form': form, 'page':page}
+    return render(request, 'index.html', context)
+def editTask(request,pk):
+    page = 'edittask'
+
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+
+        form = TaskForm(request.POST, request.FILES, instance=task)
+        if form.is_valid():
+            task = form.save()
+
+
+        context = {'task': task, 'page':page}
     return render(request, 'index.html', context)
 def deleteTask(request):
+    page = 'deletetask'
     task = Task()
     if request.method == "DELETE":
         task = Task()
 
-    context = {'task': task}
+    context = {'task': task, 'page':page}
     return render(request, 'index.html', context)
